00231e4e210eedf4faaf0dcd7fe1fe832a5418de
Jason Park <joonbin0319@gmail.com>
2023-06-18 22:50:06 +0900

    feat: adding POC for map app page

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-
-type Props = {};
-
-const page = (props: Props) => {
-  return <div>page</div>;
+const Map = () => {
+  return (
+    <div className="plot-container">
+      <iframe
+        src="/08-NW-0.0to100.0-above0.0winval100.0.html"
+        className="border-none w-screen h-screen"
+        title="Plotly Graph"
+      ></iframe>
+    </div>
+  );
 };
 
-export default page;
+export default Map;
